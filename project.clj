6d4e9be698223cb4f3ee2e7943019272848b666e
Alan Dipert <alan.dipert@gmail.com>
2010-03-19 23:01:08 -0400

    fixed swank project prob

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject minimax "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]])
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
